e9600b05b8f6bc32fdd8c0aaa12844df3361d3f7
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-01-26 16:55:16 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+void main()
+
+{
+    int a,c;
+   
+ printf("enter a number:");
+  
+  scanf("%d%d",&a,&c);
+
+ switch(a+c)
+
+ {
+    
+  case 1:
+printf("answer is one");
+   
+  break;
+     
+  case 2:
+printf("answer is two");
+  
+  break;
+    
+  case 3:printf("answer is three");
+ 
+  break; 
+   
+  default:
+printf("give proper answer");
+ 
+  break;
+     
+    
+ 
+ }
+ 
+    
+    
+  
+  
+}
