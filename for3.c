d7d448b25812f772afc4fd9f93c992fb4edf1ba7
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-01-26 16:59:47 +0530

    Create for3.c

---
+++ for3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{ 
+  
+    clrscr();
+ 
+    int a,b,c;
+ 
+    scanf("%d",&a);
+
+    for(b=1;b<=10;b++)
+
+{  
+    c=a*b;
+     
+    printf("%d*%d=%d\n",a,b,c);
+  
+}
+    getch();
+
+}    
+    
+
