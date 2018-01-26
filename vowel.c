172a9b640596b4844217dc12fcc4fbe32f3a1f6f
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-01-26 17:00:38 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void main()
+
+{
+ char a;
+
+ printf("input");
+
+ scanf("%c",&a);
+
+ switch(a)
+
+ {
+
+ case 'a':printf("vowel");
+  
+          break;
+ 
+ case 'e':printf("vowel");
+ 
+         break;
+ 
+ case 'i':printf("vowel");
+ 
+         break;
+ 
+ case 'o':printf("vowel");
+   
+         break;
+  
+ case 'u':printf("vowel");
+        
+         break; 
+
+ default:printf("consonant");
+         
+         break;
+ 
+ }
+  
+}
+          
+          
