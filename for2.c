8e8da72cabd3cc4a77af36bccdf22d386e5f58f8
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-01-26 16:58:48 +0530

    Create for2.c

---
+++ for2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{ 
+   
+    clrscr();
+ 
+    int i;
+ 
+    for(i=0;i<50;i++)
+  
+ {
+      
+    printf("%d\n",i);
+ 
+ }
+
+    getch();  
+}    
