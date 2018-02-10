7e2f3e9c6e49afc7d4c1122f90866093f054796f
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-10 11:10:33 +0530

    Create profit.c

---
+++ profit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("Enter the selling price of articles:");
+    scanf("%f",&a);
+    printf("Enter the original cost of articles:");
+    scanf("%f",&b);
+    c=b-a;
+    printf("the profite of that articles is:%f",c);
+}
