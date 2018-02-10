16ac7ce85b53e46d08ee84eb724ee5aa5b29bcac
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-10 10:58:42 +0530

    Create cost.c

---
+++ cost.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    float a,b,e,n,d;
+    printf("enter a total price :");
+    scanf("%f",&a);
+    printf("enter the quatity of apple:");
+    scanf("%f",&b);
+    printf("the  amount for apple:");
+    n=100*b;
+    printf("%f\n",n);
+    e=(a-n);
+    printf("the cost of one banana:");
+    printf("%f", d=e/12);
+}
